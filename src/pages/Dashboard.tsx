cd871c5bd01240d9599986ed97aa1fc55764ee33
juiiorsantana <157415733+juiiorsantana@users.noreply.github.com>
2025-04-11 19:58:36 -0300

    Update Dashboard.tsx
    alteração no import supabase

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/integrations/supabase/client";
 import { Header } from "@/components/layout/Header";
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
